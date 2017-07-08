86d657d9f40caa43f408aee41e7ab05a1e3f2662
Raymond Tai <raymond01tai@gmail.com>
2017-07-08 19:31:27 -0400

    Change to mysql

--- devops/devops/settings.py
+++ devops/devops/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'devops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'codex',
+        'USER': 'root',
+        'PASSWORD': 'Prin0v@',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
