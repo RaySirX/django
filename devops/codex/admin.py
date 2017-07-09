2cec2da3162f331977e3fc52de86edce8c2796ea
Raymond Tai <raymond01tai@gmail.com>
2017-07-08 20:13:31 -0400

    add SettingsEntry model

--- devops/codex/admin.py
+++ devops/codex/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class SettingsEntryAdmin(admin.ModelAdmin):
+    list_display = ("name", "value")
+    prepopulated_fields = { "slug":("name",) }
+
+admin.site.register(models.SettingsEntry, SettingsEntryAdmin)
--- devops/codex/models.py
+++ devops/codex/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class SettingsEntry(models.Model):
+    name = models.CharField(max_length=50)
+    value = models.CharField(max_length=500)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Settings Entry"
+        verbose_name_plural = "Settings Entries"
+        ordering = ["name"]
